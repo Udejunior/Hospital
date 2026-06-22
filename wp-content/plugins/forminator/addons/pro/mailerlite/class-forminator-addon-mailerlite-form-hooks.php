20b42da68787ab64ec280c00734e7ce0e0cac4f9
Udejunior <michaelokwudiriokoro@gmail.com>
2026-06-22 13:47:06 +0100

    Updated site with working menu

--- wp-content/plugins/forminator/addons/pro/mailerlite/class-forminator-addon-mailerlite-form-hooks.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Class Forminator_Mailerlite_Form_Hooks
- *
- * Hooks that used by Mailerlite Addon defined here
- */
-class Forminator_Mailerlite_Form_Hooks extends Forminator_Integration_Form_Hooks {
-}
